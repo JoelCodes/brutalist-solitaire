43b320a966ffa20691030fc6fe128cfea0984392
JoelCodes <amnshinness@yahoo.com>
2017-03-19 08:27:27 -0700

    Switched to mocha webpack.

--- test/base.test.js
+++ test/base.test.js
@@ -1,8 +1,8 @@
-const expect = require('chai').expect;
+import { expect } from 'chai';
 
 // Blank example test
 describe('test', function() {
     it('should run', function() {
-        expect(3).to.be.a('number');
+        expect(2 + 2).to.eql(4);
     });
 });
